c6096ad5b07f17ce675ab5b192c0df8dfe11dfad
InnaMnatsakanyan <inna.mnacakanyan1@gmail.com>
2026-01-09 18:51:56 +0400

    refactoring

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { syncPlugin } from './vite-plugins/sync-plugin'
 
 export default defineConfig({
-  plugins: [react(), syncPlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
